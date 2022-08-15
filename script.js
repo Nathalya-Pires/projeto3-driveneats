3f0ad627e06fd78d4ec617ccae4a162a0006f37b
Nathalya Pires <nathyohana@gmail.com>
2022-08-15 17:02:17 -0400

    final

--- script.js
+++ script.js
@@ -41,8 +41,16 @@ function marcadorDrink(botao) {
     botao.classList.add('selecionado');
 
     drink = botao;
+    nomeDrink = botao.querySelector('.itens>h1').innerText; 
+    
+    preco = botao.querySelector('.itens>h3').innerText;     //selecionei o item relativo ao preço
+    preco = preco.replace('R$','');                         //troquei o 'R$' por '' (aspas vazias, ou seja, onde tinha 'R$', agora não tem nada)
+    preco = preco.replace(',','.');                         //troquei a virgula por ponto, pois numero tem que ter ponto no JS
+    preco = Number(preco);                                  //transformei a string em numero
+    precoDrink = preco                                       //atribui o valor da variavel preço, a variavel global (que está fora da função) 'precoFood'
 
     fecharPedido();
+
 }
 
 function marcadorSweet(botao) {
@@ -54,8 +62,16 @@ function marcadorSweet(botao) {
     botao.classList.add('selecionado');
 
     sweet = botao;
+    nomeSweet = botao.querySelector('.itens>h1').innerText; 
+    
+    preco = botao.querySelector('.itens>h3').innerText;     //selecionei o item relativo ao preço
+    preco = preco.replace('R$','');                         //troquei o 'R$' por '' (aspas vazias, ou seja, onde tinha 'R$', agora não tem nada)
+    preco = preco.replace(',','.');                         //troquei a virgula por ponto, pois numero tem que ter ponto no JS
+    preco = Number(preco);                                  //transformei a string em numero
+    precoSweet = preco                                       //atribui o valor da variavel preço, a variavel global (que está fora da função) 'precoFood'
 
     fecharPedido();
+    
 }
 
 function fecharPedido() {
@@ -75,13 +91,13 @@ function fecharPedido() {
 
 function chamarWhatsapp() {
     
-    precoTotal = precoFood + precoDrink + precoSobremesa;
+    precoTotal = precoFood + precoDrink + precoSweet;
 
     let mensagem = 
         'Olá, gostaria de fazer o pedido:' + '\n- Prato: ' + nomeFood +
 		'\n- Bebida: ' + nomeDrink +
-		'\n- Sobremesa: ' + nomeSobremesa +
-		'\nTotal: R$ ' + preçoTotal.toFixed(2)
+		'\n- Sobremesa: ' + nomeSweet +
+		'\nTotal: R$ ' + precoTotal.toFixed(2)
 
     link = 'https://wa.me/5566992105957?text=' + encodeURIComponent(mensagem)
 
